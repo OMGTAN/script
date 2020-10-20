16fc67722b4659cd035192a0a914b93e7c7f2e8d
tangwei <tangwei@anji-tec.com>
2020-10-20 15:52:56 +0800

    init

---
+++ bat/BakupDataBase.bat
@@ -0,0 +1,29 @@
+:start
+
+rem "设置远程主机IP、用户名、密码、文件路径"
+set ftphost=10.108.17.27
+set username=tan
+set password=123456
+set filepath=/home/backup/database/3306
+rem "设置本地路径"
+set Ldir=D:/项目/2020/MEB立库/数据库备份
+set timevar=%time:~0,2%
+if /i %timevar% LSS 10 (
+	set timevar=0%time:~1,1%
+)
+set filename=db_%date:~0,4%-%date:~5,2%-%date:~8,2%-%timevar%.sql
+
+
+echo cd %filepath% >>sftp.text
+
+echo lcd %Ldir% >>sftp.text
+
+echo get %filename% >>sftp.text
+
+echo bye >>sftp.text
+
+
+
+psftp -P 22 %username%@%ftphost% -pw %password% -b sftp.text
+
+del sftp.text
