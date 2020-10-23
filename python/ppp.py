590a47a599d817434d5784e806d16491e32ae9a8
tangwei <tangwei@anji-tec.com>
2020-10-23 16:54:15 +0800

    pa

--- python/ppp.py
+++ python/ppp.py
@@ -1,22 +1,52 @@
-import re
-
 import requests
+from fake_useragent import UserAgent
 
-# 知乎有反爬虫，加入http headers伪装浏览器
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
-    "Connection": "keep-alive",
-    "Accept": "text/html,application/json,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "zh-CN,zh;q=0.8"}
+############## 数据存储
+import time
 
-# 知乎问题id
-question_id = 406321189
-interval = 20
-offset = 0
-rank = 100
-novels_count = dict()
 
-url = f'https://www.zhihu.com/api/v4/questions/{question_id}/answers?include=content&limit={interval}&offset={offset}&sort_by=default'
-html = requests.get(url, headers=headers)
-answers = html.json()['data']
-print(answers)
+##################################
+
+class ZhihuOne(object):
+
+    def __init__(self,totle):
+
+        self._offset = 0
+        self._totle = totle
+        #self._ua = UserAgent()
+
+
+    def run(self):
+
+        print("正在抓取 {} 数据".format(self._offset))
+        headers = {
+            "upgrade-insecure-requests":"1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64)"
+        }
+        # 406321189
+        with requests.Session() as s:
+            try:
+                with s.get("https://www.zhihu.com/api/v4/questions/406321189/answers?include=comment_count,content,voteup_count,reshipment_settings,is_author,voting,is_thanked,is_nothelp;data[*].mark_infos[*].url;data[*].author.follower_count,badge[*].topics&limit=5&offset={}&sort_by=default".format(self._offset),headers=headers,timeout=3) as rep:
+                    data =  rep.json()
+                    if data:
+                        print(data["data"][0])
+            except Exception as e:
+                print(e.args)
+
+            finally:
+
+                if self._offset <= self._totle:
+                    self._offset  = self._offset + 5 # 每次+5
+                    print("防止被办，休息3s")
+                    time.sleep(3)
+                    self.run()
+                else:
+                    print("所有数据获取完毕")
+
+
+
+
+if __name__ == '__main__':
+    # 偏移量是0,5,10   i=1  (i-1)*5
+    zhi = ZhihuOne(1084)  
+    zhi.run()
