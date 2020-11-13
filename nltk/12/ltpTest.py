7b9a9f62fc094f35c1451e9f6e270ed71dfdddb2
tangwei <tangwei@anji-tec.com>
2020-11-13 16:31:22 +0800

    ml

--- nltk/12/ltpTest.py
+++ nltk/12/ltpTest.py
@@ -1,30 +1,33 @@
+# coding:utf-8
+ 
 from ltp import LTP
 
 ltp = LTP()
 sents = ltp.sent_split(["他叫汤姆去拿外衣。", "汤姆生病了。他去了医院。"])
 
 for sent in sents:
+    l1 = [sent]
+    # l1.append(sent)
     #分词
-    seg, hidden = ltp.seg(list(sent))
+    seg, hidden = ltp.seg(l1)
     print(seg)
     #词性标注
     print(ltp.pos(hidden))
     #命名实体识别
     ner = ltp.ner(hidden)
-    print(ner)
-    if(len(ner[0]) != 0):
+    print("命名实体识别", ner)
+    if (len(ner[0]) != 0):
         tag, start, end = ner[0][0]
         print(tag, ":", "".join(seg[0][start:end + 1]))
-    #语意角色标注
+    #语义角色标注
     srl = ltp.srl(hidden)
-    print(srl)
+    print("语义角色标注", srl)
     #依存句法分析
     dep = ltp.dep(hidden)
-    print(dep)
-    #语意依存分析
+    print("依存句法分析", dep)
+    #语义依存分析
     sdp = ltp.sdp(hidden, graph=False)
-    print(sdp)
+    print("语义依存分析", sdp)
     #图
     sdp = ltp.sdp(hidden, graph=True)
-    print(sdp)
-
+    print("语义依存分析 图", sdp)
